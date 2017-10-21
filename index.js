5883365ef5573ec898d157a198bd05453819e923
Megan Jacobs <mjay815@hotmail.com>
2017-10-21 09:36:33 -0400

    node added to start in package.json

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 
-app.listen(PORT, function() {
-	console.log("Listening on port: " + PORT);
-});
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,5 +13,10 @@ app.use(bodyParser.json());
 
 app.get("/", function(req, res){
 	res.send("some text");
+});
+
+
+app.listen(PORT, function() {
+	console.log("Listening on port: " + PORT);
 });
 
